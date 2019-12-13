9116fc567ab7eeecc3e656388ee170066f7cb035
Kevin Berendsen <kevinberendsen@kramphub.com>
2019-12-13 16:13:16 +0100

    Implemented the configuration with the new methods

--- src/main/java/jsr381/tck/ri/DeepNettsJSR381Configuration.java
+++ src/main/java/jsr381/tck/ri/DeepNettsJSR381Configuration.java
@@ -1,16 +1,26 @@
 package jsr381.tck.ri;
 
 import jsr381.tck.spi.JSR381Configuration;
+import visrec.ri.BufferedImageFactory;
 
+import javax.visrec.ImageFactory;
 import javax.visrec.ml.classification.BinaryClassifier;
 import javax.visrec.ml.classification.ImageClassifier;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class DeepNettsJSR381Configuration implements JSR381Configuration {
     @Override
-    public ImageClassifier.BuildingBlock getABImageClassificationBuildingBlock() {
+    public ImageClassifier.BuildingBlock getABImageClassificationBuildingBlock(ImageClassifier.Builder builder) {
+        return getABImageClassificationBuilder(builder).getBuildingBlock();
+    }
+
+    @Override
+    public Map<String, Object> getABImageClassificationConfigMap(Map<String, Object> configMap) {
         URL directoryUrl = DeepNettsJSR381Configuration.class.getClassLoader().getResource("12-image-classifier");
         if (directoryUrl == null)
             throw new IllegalStateException("Unable to find 12-image-classifier directory in resources.");
@@ -25,22 +35,43 @@ public class DeepNettsJSR381Configuration implements JSR381Configuration {
             throw new IllegalStateException("Unable to create temporary model file", e);
         }
 
-        ImageClassifier.BuildingBlock buildingBlock = ImageClassifier.builder()
-                .imageHeight(28)
-                .imageWidth(28)
+        configMap.put("labelsFile", new File(directory, "labels.txt"));
+        configMap.put("trainingsFile", new File(directory, "training_data/train.txt"));
+        configMap.put("networkArchitecture", new File(directory, "arch.json"));
+        configMap.put("modelFile", temporaryOutputFile);
+        return configMap;
+    }
+
+    @Override
+    public ImageClassifier.Builder getABImageClassificationBuilder(ImageClassifier.Builder builder) {
+        URL directoryUrl = DeepNettsJSR381Configuration.class.getClassLoader().getResource("12-image-classifier");
+        if (directoryUrl == null)
+            throw new IllegalStateException("Unable to find 12-image-classifier directory in resources.");
+
+        File directory = new File(directoryUrl.getFile());
+
+        File temporaryOutputFile = new File("ab.dnet");
+        try {
+            if (!temporaryOutputFile.exists() && temporaryOutputFile.createNewFile())
+                temporaryOutputFile.deleteOnExit();
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to create temporary model file", e);
+        }
+
+        return builder
                 .labelsFile(new File(directory, "labels.txt"))
                 .trainingsFile(new File(directory, "training_data/train.txt"))
                 .networkArchitecture(new File(directory, "arch.json"))
-                .modelFile(temporaryOutputFile)
-                .maxError(0.4f)
-                .maxEpochs(100)
-                .learningRate(0.01f)
-                .getBuildingBlock();
-        return buildingBlock;
+                .modelFile(temporaryOutputFile);
     }
 
     @Override
     public BinaryClassifier.BuildingBlock getSpamBinaryClassificationBuildingBlock() {
         throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    @Override
+    public List<ImageFactory<?>> getImageFactories() {
+        return Arrays.asList(new BufferedImageFactory());
     }
 }
