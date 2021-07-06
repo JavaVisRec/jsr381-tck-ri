032d60da06d411fa28a584076da10e0c1806e10e
kevto <berendsen.kevin@gmail.com>
2021-07-06 20:02:13 +0200

    Fixing tck

--- src/main/java/jsr381/tck/ri/DeepNettsJSR381Configuration.java
+++ src/main/java/jsr381/tck/ri/DeepNettsJSR381Configuration.java
@@ -1,73 +1,63 @@
 package jsr381.tck.ri;
 
 import jsr381.tck.spi.JSR381Configuration;
-import visrec.ri.BufferedImageFactory;
 
 import javax.visrec.ImageFactory;
-import javax.visrec.ml.classification.BinaryClassifier;
-import javax.visrec.ml.classification.ImageClassifier;
+import javax.visrec.ml.classification.NeuralNetBinaryClassifier;
+import javax.visrec.ml.classification.NeuralNetImageClassifier;
+import javax.visrec.ri.BufferedImageFactory;
+import javax.visrec.ri.spi.BufferedImageClassifierFactory;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class DeepNettsJSR381Configuration implements JSR381Configuration {
     @Override
-    public ImageClassifier.BuildingBlock getABImageClassificationBuildingBlock(ImageClassifier.Builder builder) {
+    public NeuralNetImageClassifier.BuildingBlock<BufferedImage> getABImageClassificationBuildingBlock(NeuralNetImageClassifier.Builder<BufferedImage> builder) {
         return getABImageClassificationBuilder(builder).getBuildingBlock();
     }
 
     @Override
-    public Map<String, Object> getABImageClassificationConfigMap(Map<String, Object> configMap) {
+    public NeuralNetImageClassifier.Builder<BufferedImage> getABImageClassificationBuilder(NeuralNetImageClassifier.Builder<BufferedImage> builder) {
         URL directoryUrl = DeepNettsJSR381Configuration.class.getClassLoader().getResource("12-image-classifier");
         if (directoryUrl == null)
             throw new IllegalStateException("Unable to find 12-image-classifier directory in resources.");
 
-        File directory = new File(directoryUrl.getFile());
+        var directory = new File(directoryUrl.getFile()).getAbsolutePath();
 
-        File temporaryOutputFile = new File("ab.dnet");
+        var tempOutputModel = Path.of("ab.dnet");
+        var tempOutputFile = tempOutputModel.toFile();
         try {
-            if (!temporaryOutputFile.exists() && temporaryOutputFile.createNewFile())
-                temporaryOutputFile.deleteOnExit();
-        } catch (IOException e) {
-            throw new IllegalStateException("Unable to create temporary model file", e);
-        }
-
-        configMap.put("labelsFile", new File(directory, "labels.txt"));
-        configMap.put("trainingsFile", new File(directory, "training_data/train.txt"));
-        configMap.put("networkArchitecture", new File(directory, "arch.json"));
-        configMap.put("modelFile", temporaryOutputFile);
-        return configMap;
-    }
-
-    @Override
-    public ImageClassifier.Builder getABImageClassificationBuilder(ImageClassifier.Builder builder) {
-        URL directoryUrl = DeepNettsJSR381Configuration.class.getClassLoader().getResource("12-image-classifier");
-        if (directoryUrl == null)
-            throw new IllegalStateException("Unable to find 12-image-classifier directory in resources.");
-
-        File directory = new File(directoryUrl.getFile());
-
-        File temporaryOutputFile = new File("ab.dnet");
-        try {
-            if (!temporaryOutputFile.exists() && temporaryOutputFile.createNewFile())
-                temporaryOutputFile.deleteOnExit();
+            if (!tempOutputFile.exists() && tempOutputFile.createNewFile())
+                tempOutputFile.deleteOnExit();
         } catch (IOException e) {
             throw new IllegalStateException("Unable to create temporary model file", e);
         }
 
         return builder
-                .labelsFile(new File(directory, "labels.txt"))
-                .trainingsFile(new File(directory, "training_data/train.txt"))
-                .networkArchitecture(new File(directory, "arch.json"))
-                .modelFile(temporaryOutputFile);
+                .labelsFile(Paths.get(directory, "labels.txt"))
+                .trainingFile(Paths.get(directory, "training_data/train.txt"))
+                .networkArchitecture(Paths.get(directory, "arch.json"))
+                .exportModel(tempOutputModel);
     }
 
     @Override
-    public BinaryClassifier.BuildingBlock getSpamBinaryClassificationBuildingBlock() {
-        throw new UnsupportedOperationException("not implemented yet");
+    public NeuralNetBinaryClassifier.BuildingBlock<float[]> getSpamBinaryClassificationBuildingBlock(NeuralNetBinaryClassifier.Builder<float[]> builder) {
+        return getSpamBinaryClassificationBuilder(builder).getBuildingBlock();
+    }
+
+    @Override
+    public NeuralNetBinaryClassifier.Builder<float[]> getSpamBinaryClassificationBuilder(NeuralNetBinaryClassifier.Builder<float[]> builder) {
+        URL spamCsvResource = DeepNettsJSR381Configuration.class.getClassLoader().getResource("spam.csv");
+        if (spamCsvResource == null)
+            throw new IllegalStateException("Unable to find spam.csv in resources.");
+        var trainingPath = Path.of(new File(spamCsvResource.getFile()).getAbsolutePath());
+        return builder.trainingPath(trainingPath);
     }
 
     @Override
